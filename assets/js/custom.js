ad19c6f39c1dfd9ad483e6311a87869e36839d08
Muhammad Yota <muhammadyota86@gmail.com>
2022-03-07 14:07:51 +0700

    fix reponsive halaman index html

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,31 @@
 (function ($) {
 
+  $(document).ready(function () {
+    $("#testimonial-slider").owlCarousel({
+      items: 2,
+      itemsDesktop: [1000, 2],
+      itemsDesktopSmall: [990, 1],
+      itemsTablet: [768, 1],
+      pagination: true,
+      navigation: false,
+      navigationText: ["", ""],
+      slideSpeed: 1000,
+      autoPlay: true
+    });
+
+  });
+
   // Header Type = Fixed
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     var box = $('.services .header-text').height();
     var header = $('header').height();
-
     if (scroll >= box - header) {
       $("header").addClass("background-header");
     } else {
       $("header").removeClass("background-header");
     }
   });
-
 
   $('.loop').owlCarousel({
     center: true,
@@ -34,110 +47,8 @@
     }
   });
 
-  $("#modal_trigger").leanModal({
-    top: 100,
-    overlay: 0.6,
-    closeButton: ".modal_close"
-  });
-
-  // Acc
-  $(document).on("click", ".naccs .menu div", function () {
-    var numberIndex = $(this).index();
-
-    if (!$(this).is("active")) {
-      $(".naccs .menu div").removeClass("active");
-      $(".naccs ul li").removeClass("active");
-
-      $(this).addClass("active");
-      $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
-
-      var listItemHeight = $(".naccs ul")
-        .find("li:eq(" + numberIndex + ")")
-        .innerHeight();
-      $(".naccs ul").height(listItemHeight + "px");
-    }
-  });
-
-
-  // Menu Dropdown Toggle
-  if ($('.menu-trigger').length) {
-    $(".menu-trigger").on('click', function () {
-      $(this).toggleClass('active');
-      $('.header-area .nav').slideToggle(200);
-    });
-  }
-
-
-  // Menu elevator animation
-  $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        var width = $(window).width();
-        if (width < 991) {
-          $('.menu-trigger').removeClass('active');
-          $('.header-area .nav').slideUp(200);
-        }
-        $('html,body').animate({
-          scrollTop: (target.offset().top) + 1
-        }, 700);
-        return false;
-      }
-    }
-  });
-
-  $(document).ready(function () {
-
-    //smoothscroll
-    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
-      e.preventDefault();
-      $(document).off("scroll");
-
-      $('.scroll-to-section a').each(function () {
-        $(this).removeClass('active');
-      })
-      $(this).addClass('active');
-
-      var target = this.hash,
-        menu = target;
-      var target = $(this.hash);
-      $('html, body').stop().animate({
-        scrollTop: (target.offset().top) + 1
-      }, 500, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
-      });
-    });
-  });
-
-
-  // Acc
-  $(document).on("click", ".naccs .menu div", function () {
-    var numberIndex = $(this).index();
-
-    if (!$(this).is("active")) {
-      $(".naccs .menu div").removeClass("active");
-      $(".naccs ul li").removeClass("active");
-
-      $(this).addClass("active");
-      $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
-
-      var listItemHeight = $(".naccs ul")
-        .find("li:eq(" + numberIndex + ")")
-        .innerHeight();
-      $(".naccs ul").height(listItemHeight + "px");
-    }
-  });
-
-
   // Page loading animation
   $(window).on('load', function () {
-
     $('#js-preloader').addClass('loaded');
-
   });
-
-
-
 })(window.jQuery);
