3e34b90a303234bb14badf3e14dbf298005cdb01
Muhammad Yota <muhammadyota86@gmail.com>
2022-03-08 20:39:47 +0700

    update responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -47,6 +47,36 @@
     }
   });
 
+  $("#modal_trigger").leanModal({
+    top: 100,
+    overlay: 0.6,
+    closeButton: ".modal_close"
+  });
+  // Menu Dropdown Toggle
+  if ($('.menu-trigger').length) {
+    $(".menu-trigger").on('click', function () {
+      $(this).toggleClass('active');
+      $('.header-area .nav').slideToggle(200);
+    });
+  }
+  // Menu elevator animation
+  $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        var width = $(window).width();
+        if (width < 991) {
+          $('.menu-trigger').removeClass('active');
+          $('.header-area .nav').slideUp(200);
+        }
+        $('html,body').animate({
+          scrollTop: (target.offset().top) + 1
+        }, 700);
+        return false;
+      }
+    }
+  });
   // Page loading animation
   $(window).on('load', function () {
     $('#js-preloader').addClass('loaded');
