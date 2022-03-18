b062a1c27532348fbe1052371f0881c0ff696508
Muhammad Yota <muhammadyota86@gmail.com>
2022-03-18 21:30:40 +0700

    update banner dan metatag

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,4 +21,16 @@
     $('#js-preloader').addClass('loaded');
   });
 
+  $("#testimonial-slider").owlCarousel({
+    items: 2,
+    itemsDesktop: [1000, 2],
+    itemsDesktopSmall: [990, 1],
+    itemsTablet: [768, 1],
+    pagination: true,
+    navigation: false,
+    navigationText: ["", ""],
+    slideSpeed: 1000,
+    autoPlay: true
+  });
+
 })(jQuery);
