4bef5a00b88ce35f20d771196d56fe35c89de418
Muhammad Yota <muhammadyota86@gmail.com>
2022-06-26 12:04:23 +0700

    update tampilan halaman gojobs

---
+++ assets/js/jobs-page.js
@@ -0,0 +1,6 @@
+let menu_mobile = document.getElementById("menu-mobile");
+
+menu_mobile.addEventListener("click", function (event) {
+  let navbar_link_mobile = document.getElementById("navbar-link-mobile");
+  navbar_link_mobile.classList.toggle("d-flex");
+});
